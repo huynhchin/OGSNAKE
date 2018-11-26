2674e08fd52251e3789134440d90415c496c29db
Jewel Ito <jewelito@jewels-macbook-pro.local>
2018-11-26 13:39:12 -0800

    initial snake game

--- iOS Final Project/GameViewController.swift
+++ iOS Final Project/GameViewController.swift
@@ -27,7 +27,7 @@ class GameViewController: UIViewController {
             
             view.ignoresSiblingOrder = true
             
-            view.showsFPS = false
+            view.showsFPS = true
             view.showsNodeCount = false
         }
     }
